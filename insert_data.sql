32c849fe8aa6468b6ecd522fef21074722fbc19b
mvdholen <91984324+mvdholen@users.noreply.github.com>
2025-05-05 15:25:19 +0200

    wip_commit : 250505_1525

--- insert_data.sql
+++ insert_data.sql
@@ -1,18 +1,19 @@
 -- PGSQL CODE TO UPLOAD DATA FROM ADDRESS.CSV TO ADDRESS TABLE
 TRUNCATE TABLE address CASCADE;
-
+ALTER SEQUENCE address_id_seq RESTART WITH 1;
 COPY address (street, house_number, postal_code, municipality)
 FROM 'C:/_MaRn/syntra/projects/python_eindopdracht/proof_of_concept_folium/addresses/addresses.csv'
 DELIMITER ','
 CSV HEADER
 ;
 
-/*
 SELECT * FROM address
-LIMIT 10;
+LIMIT 10
+/*
 */
+;
 
-SELECT * FROM address ;
+
 
 
 
